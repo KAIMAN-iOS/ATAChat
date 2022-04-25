cde271f29b5202aa10e9963e2356dc04f034f5de
jerometonnelier <jerometonnelier@gmx.fr>
2022-04-25 14:30:31 +0200

    single user token

--- Sources/ATAChat/Managers/TokenController.swift
+++ Sources/ATAChat/Managers/TokenController.swift
@@ -11,8 +11,9 @@ import FirebaseFirestore
 public struct TokenController {
     static private let db = Firestore.firestore()
     static private var userReference: CollectionReference { db.collection("user") }
+    
     public static func update(token: String, for userId: String) {
-        userReference.document(userId).updateData(["notificationTokens" : FieldValue.arrayUnion([token])])
+        userReference.document(userId).updateData(["notificationTokens" : [token]])
     }
     public static func remove(token: String, for userId: String) {
         userReference.document(userId).updateData(["notificationTokens" : FieldValue.arrayRemove([token])])
